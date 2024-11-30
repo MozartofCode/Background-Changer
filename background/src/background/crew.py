a8cb021113e49b8d2e3ff0b7cfe8f75de071e69f
MozartofCode <bertan.berker@gmail.com>
2024-11-30 15:57:06 -0500

    Created a downloader agent

--- background/src/background/crew.py
+++ background/src/background/crew.py
@@ -19,33 +19,17 @@ class BackgroundCrew():
 			verbose=True
 		)
 
-	@agent
-	def reporting_analyst(self) -> Agent:
-		return Agent(
-			config=self.agents_config['reporting_analyst'],
-			verbose=True
-		)
-
 	@task
 	def research_task(self) -> Task:
 		return Task(
 			config=self.tasks_config['research_task'],
 		)
 
-	@task
-	def reporting_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['reporting_task'],
-			output_file='report.md'
-		)
-
 	@crew
 	def crew(self) -> Crew:
 		"""Creates the Background crew"""
 		return Crew(
-			agents=self.agents, # Automatically created by the @agent decorator
-			tasks=self.tasks, # Automatically created by the @task decorator
-			process=Process.sequential,
+			agents=self.agents,
+			tasks=self.tasks,
 			verbose=True,
-			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
 		)
